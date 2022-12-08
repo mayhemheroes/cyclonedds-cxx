dcccdd1c40e48e779030a7df3e2e32bc647a32cc
Jon-FAS <jhersack@forallsecure.com>
2022-12-08 07:35:19 -0500

    appears to work?

--- examples/fuzz_pub_topic/fuzz_pub_topic.cpp
+++ examples/fuzz_pub_topic/fuzz_pub_topic.cpp
@@ -22,21 +22,81 @@
 
 using namespace org::eclipse::cyclonedds;
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-        std::cout << "=== [Publisher] Create writer." << std::endl;
+
+        // child process because return value zero
+        if (fork() == 0){
+        // std::cout << "=== [Subscriber] Create reader." << std::endl;
 
         /* First, a domain participant is needed.
          * Create one on the default domain. */
         dds::domain::DomainParticipant participant(domain::default_id());
-        
-        char *str = NULL;
-        if ((str = (char *)malloc(size + 1)) == NULL)
-            return EXIT_FAILURE;
 
-        memcpy(str, data, size);
-        str[size] = '\0';
+        /* To subscribe to something, a topic is needed. */
+        dds::topic::Topic<FuzzData::Msg> topic(participant, "FuzzData_Msg");
+
+        /* A reader also needs a subscriber. */
+        dds::sub::Subscriber subscriber(participant);
+
+        /* Now, the reader can be created to subscribe to a HelloWorld message. */
+        dds::sub::DataReader<FuzzData::Msg> reader(subscriber, topic);
+
+        /* Poll until a message has been read.
+         * It isn't really recommended to do this kind wait in a polling loop.
+         * It's done here just to illustrate the easiest way to get data.
+         * Please take a look at Listeners and WaitSets for much better
+         * solutions, albeit somewhat more elaborate ones. */
+        // std::cout << "=== [Subscriber] Wait for message." << std::endl;
+        bool poll = true;
+        while (poll) {
+            /* For this example, the reader will return a set of messages (aka
+             * Samples). There are other ways of getting samples from reader.
+             * See the various read() and take() functions that are present. */
+            dds::sub::LoanedSamples<FuzzData::Msg> samples;
+
+            /* Try taking samples from the reader. */
+            samples = reader.take();
+
+            /* Are samples read? */
+            if (samples.length() > 0) {
+                /* Use an iterator to run over the set of samples. */
+                dds::sub::LoanedSamples<FuzzData::Msg>::const_iterator sample_iter;
+                for (sample_iter = samples.begin();
+                     sample_iter < samples.end();
+                     ++sample_iter) {
+                    /* Get the message and sample information. */
+                    const FuzzData::Msg& msg = sample_iter->data();
+                    const dds::sub::SampleInfo& info = sample_iter->info();
+
+                    /* Sometimes a sample is read, only to indicate a data
+                     * state change (which can be found in the info). If
+                     * that's the case, only the key value of the sample
+                     * is set. The other data parts are not.
+                     * Check if this sample has valid data. */
+                    if (info.valid()) {
+                        // std::cout << "=== [Subscriber] Message received:" << std::endl;
+                        // std::cout << "    userID  : " << msg.userID() << std::endl;
+                        // std::cout << "    Message : \"" << msg.message() << "\"" << std::endl;
+
+                        /* Only 1 message is expected in this example. */
+                        poll = false;
+                    }
+                }
+            } else {
+                std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            }
+        }
+        }
+        // parent process because return value non-zero.
+        else{
+        // std::cout << "=== [Publisher] Create writer." << std::endl;
+
+        /* First, a domain participant is needed.
+         * Create one on the default domain. */
+        dds::domain::DomainParticipant participant(domain::default_id());
 
         /* To publish something, a topic is needed. */
-        dds::topic::Topic<FuzzData::Msg> topic(participant, str);
+        //dds::topic::Topic<FuzzData::Msg> topic(participant, str);
+        dds::topic::Topic<FuzzData::Msg> topic(participant, "FuzzData_Msg");
 
         /* A writer also needs a publisher. */
         dds::pub::Publisher publisher(participant);
@@ -50,16 +110,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
          * really recommended to do a wait in a polling loop, however.
          * Please take a look at Listeners and WaitSets for much better
          * solutions, albeit somewhat more elaborate ones. */
-        std::cout << "=== [Publisher] Waiting for subscriber." << std::endl;
-        while (writer.publication_matched_status().current_count() == 0) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
-        }
+        // std::cout << "=== [Publisher] Waiting for subscriber." << std::endl;
+        // while (writer.publication_matched_status().current_count() == 0) {
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        // }
 
         /* Create a message to write. */
-        FuzzData::Msg msg(1, "Hello World");
+
+        std::string str(reinterpret_cast<const char*>(data), size);
+
+        FuzzData::Msg msg(1, str);
 
         /* Write the message. */
-        std::cout << "=== [Publisher] Write sample." << std::endl;
+        // std::cout << "=== [Publisher] Write sample." << std::endl;
         writer.write(msg);
 
         /* With a normal configuration (see dds::pub::qos::DataWriterQos
@@ -68,12 +131,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
          * Wait for the subscriber to have stopped to be sure it received the
          * message. Again, not normally necessary and not recommended to do
          * this in a polling loop. */
-        std::cout << "=== [Publisher] Waiting for sample to be accepted." << std::endl;
+        // std::cout << "=== [Publisher] Waiting for sample to be accepted." << std::endl;
         while (writer.publication_matched_status().current_count() > 0) {
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     
-    std::cout << "=== [Publisher] Done." << std::endl;
-
+        // std::cout << "=== [Publisher] Done." << std::endl;
+        }
+           
     return EXIT_SUCCESS;
 }
